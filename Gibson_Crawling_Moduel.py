5a32ef2d685d31488540821af3fecf248ab0a11c
junghokang <jhkang1517@gmail.com>
2019-06-10 17:53:35 +0900

    0610 수업

--- Gibson_Crawling_Moduel.py
+++ Gibson_Crawling_Moduel.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[28]:
+# In[1]:
 
 
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 import os
+import pandas as pd
         
-introduction = """Les-Paul, SG, ES, Acoustic, Designer, Bass 를 'crwaling( )'안에 입력하세요.
-입력한 값의 모든 깁슨 모델을 제공합니다. """
+intro = """Les-Paul, SG, ES, Acoustic, Designer, Bass 를 'crl.run( )'안에 입력하세요.
+입력한 값에 대한 깁슨 홈페이지에 있는 모든 깁슨 모델을 제공합니다."""
 maker = 'jungho kang'
 
-BASE_DIR = os.getcwd()
+BASE_DIR = os.getcwd() # os명령어. get current work directory? 현재 작업 주소
 
-class crl:
+class crl(object):
     # def __init__(self, base_dir = BASE_DIR):
     def __init__(self, base_dir):
         self.base_dir = BASE_DIR
     
     def run(model):
                  
-        PICTURE_DIR = os.path.join(BASE_DIR, 'guitar_pictures')
+        PICTURE_DIR = os.path.join(BASE_DIR, 'guitar_pictures_{}'.format(model))
         if not os.path.exists(PICTURE_DIR):
             os.makedirs(PICTURE_DIR) # 만약 경로가 없으면, 생성하겠습니다.
     
         guitar_list = [] # 새로운 빈 리스트를 생성합니다.
         gibson_usl = "https://www.gibson.com/Guitars/"+model 
-#'SG','ES','Acoustic', 'Designer', 'Bass' 를 넣어도 똑같이 작동합니다.
 # params = {
 #     'Guitars': 'Guitars',
 #     'Les-Paul': 'Les-Paul'
@@ -46,7 +46,7 @@ class crl:
             guitar_dict = {}
 
             a_tag = div.find('h4').find('a')
-        # print(a_tag)
+    # print(a_tag)
             guitar_dict['link'] = 'https://www.gibson.com'+a_tag['href']
             guitar_dict['name'] = a_tag.text
     # print(a_tag['href'])
@@ -72,18 +72,13 @@ class crl:
             with open(guitar_picture_path, 'wb') as f:
                 f.write(img_resp.content)
     
-    # 구현 중. 상세페이지 내용 가져오기 
+    # fix plz
     # detail_resp = requests.get('https://www.gibson.com'+a_tag['href'])
     # detail_soup = BeautifulSoup(detail_resp.content, 'html.parser')
     # detail_soup
     # detail_contents = soup.find('div', class_='gibson-card py-4')
     # detail_contents
     
-        return pprint(guitar_list)
-
-
-# In[ ]:
-
-
-
+        
+        return guitar_list
 
